fb49481cae6f3fec09bf6c729eccf86bde8f002d
hafidnurkholis <itsmaehere@gmail.com>
2023-04-16 17:20:22 +0700

    feat: add external url component because NuxtLink did't work

---
+++ components/content/externalLink.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  href: {
+    type: String,
+    default: ''
+  },
+  target: {
+    type: String,
+    default: '_blank',
+    required: false
+  }
+})
+</script>
+
+<template>
+  <a :href="href" :target="target">
+    link.
+  </a>
+</template>
