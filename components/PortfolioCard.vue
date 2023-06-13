a102d198be4718152bc1efd6f381297971aab188
hafidnurkholis <itsmaehere@gmail.com>
2023-06-13 08:52:27 +0700

    :bug: fix: cannot redirect to live preview of portfolio in mobile

--- components/PortfolioCard.vue
+++ components/PortfolioCard.vue
@@ -9,42 +9,34 @@ defineProps<{
     thumbnail: string;
   };
 }>();
+
+const closePopUp = (event: Event) => {
+  event.stopPropagation()
+}
 </script>
 
 <template>
-  <div
-    external="true"
-    :href="article.link"
-    target="_blank"
-    class="flex flex-col justify-between h-full hover:bg-black/5 p-4 dark:hover:bg-white/5"
-  >
-    <div class="mb-4">
-      <h2 class="font-semibold dark:text-white mb-2">
-        {{ article.title }}
-      </h2>
-      <p class="dark:text-gray-300 text-sm">
-        {{ article.description }}
-      </p>
-    </div>
-    <div
-      class="bg-black/20 place-items-center"
-      :class="{ 'grid fixed inset-0 ': show === true }"
-      @click="show = false"
-    >
-      <NuxtImg
-        :src="`/portfolio/${article.thumbnail}`"
-        format="webp"
-        class="aspect-video rounded object-cover"
-        :class="{
-          'md:hidden': show === false,
-          'w-1/2 aspect-video': show === true,
-        }"
-      />
+  <div class="h-full p-4 hover:bg-black/5 dark:hover:bg-white/5">
+    <NuxtLink :external="true" :href="article.link" target="_blank"
+      class="flex flex-col justify-between md:pointer-events-none">
+      <div class="mb-4">
+        <h2 class="font-semibold dark:text-white mb-2">
+          {{ article.title }}
+        </h2>
+        <p class="dark:text-gray-300 text-sm">
+          {{ article.description }}
+        </p>
+      </div>
+    </NuxtLink>
+    <div class="bg-black/20 place-items-center pointer-events-auto" :class="{ 'grid fixed inset-0 ': show === true }"
+      @click="show = false">
+      <NuxtImg :src="`/portfolio/${article.thumbnail}`" format="webp" class="aspect-video rounded object-cover" :class="{
+        'md:hidden': show === false,
+        'w-1/2 aspect-video': show === true,
+      }" />
     </div>
     <div class="hidden gap-1 md:flex">
-      <TheButton :variant="'primary'" @buttonClicked="show = true"
-        >Show</TheButton
-      >
+      <TheButton :variant="'primary'" @buttonClicked="show = true">Show</TheButton>
       <TheButton :variant="'success'">
         <NuxtLink :href="article.link" :external="true" target="_blank">
           Live Preview
