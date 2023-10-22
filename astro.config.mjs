3e03c21e238bca90e71385fd125ba37bbc4afcec
kholishafid <hafidnkholis@gmail.com>
2023-10-23 05:47:51 +0700

    feat: add astro sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
+import sitemap from "@astrojs/sitemap"
 import mdx from "@astrojs/mdx";
+import robotsTxt from 'astro-robots-txt'
 import { remarkReadingTime } from './src/helper/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), mdx()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
   image: {
     domains: ["https://source.unsplash.com",],
-    remotePatterns: [{ protocol: "https" }],  
-  }
+    remotePatterns: [{ protocol: "https" }],
+  },
+  site: 'https://kholishafid.github.io',
+  integrations: [tailwind(), vue(), mdx(), sitemap(), robotsTxt()],
 });
